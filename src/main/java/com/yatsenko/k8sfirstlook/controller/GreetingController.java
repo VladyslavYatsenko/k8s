950483e199d5603e843b25abcc03dcae5d898288
Vladyslav Yatsenko <Vladyslav_Yatsenko@epam.com>
2021-10-11 17:42:24 +0300

    'init'

---
+++ src/main/java/com/yatsenko/k8sfirstlook/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.yatsenko.k8sfirstlook.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/yatsenko/k8sfirstlook/K8sFirstLookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatsenko.k8sfirstlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sFirstLookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
